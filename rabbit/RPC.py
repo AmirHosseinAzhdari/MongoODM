5eb81e72da28e5e6562286b3bd029605e36b8e46
Ramin_Azali <raminazali8@gmail.com>
2021-02-21 11:03:14 +0330

    rf and Rabbit some code

--- rabbit/RPC.py
+++ rabbit/RPC.py
@@ -4,8 +4,10 @@ from functools import partial
 from aio_pika import *
 import asyncio
 
+
 class RPCClass:
 
+    # -------------------------------------------------------------------------------------------------
     @staticmethod
     async def rpc_consumer(exchange_type=None, queue_name=None, call_back_method=None, loop=None):
         connection = await connect(
@@ -15,13 +17,19 @@ class RPCClass:
         channel = await connection.channel()
 
         queue = await channel.declare_queue(queue_name)
+    # -------------------------------------------------------------------------------------------------
 
         await queue.consume(partial(
             call_back_method, channel.default_exchange)
         )
 
+    # -------------------------------------------------------------------------------------------------
     @staticmethod
-    async def rpc_client(message_body,routing_key=None, callback_queue=None, loop=None):
+    async def rpc_client(message_body, routing_key=None, callback_queue=None, loop=None):
+        if routing_key is None:
+            return "routing key can not be null"
+    # -------------------------------------------------------------------------------------------------
+
         connection = await connect(
             "amqp://user:example@10.10.10.20:5672/", loop=loop
         )
@@ -29,7 +37,7 @@ class RPCClass:
         channel = await connection.channel()
 
         correlation_id = str(uuid.uuid4())
-
+    # -------------------------------------------------------------------------------------------------
         await channel.default_exchange.publish(
             Message(
                 body=str(message_body).encode(),
@@ -44,3 +52,4 @@ class RPCClass:
         )
 
         await connection.close()
+    # -------------------------------------------------------------------------------------------------
