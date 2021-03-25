f3ca1c63e6defed68659ef2ccbe306d7f35b7e3d
azhdari <dev.amirhossein.azhdari@outlook.com>
2021-03-25 13:28:57 +0430

    update foreign key and AsyncAPIView class

--- rabbit/consumer.py
+++ rabbit/consumer.py
@@ -1,14 +1,9 @@
 from aio_pika import *
 import asyncio
 import os
-from dotenv import load_dotenv
-
-from WorkSpaceMS.settings import QUEUE_NAME
-
-load_dotenv()
 
 
-async def consumer(exchange_name=None, exchange_type=None, queue_name=QUEUE_NAME, durable=False,
+async def consumer(exchange_name=None, exchange_type=None, queue_name=os.getenv('QUEUE_NAME'), durable=False,
                    callback=None, broker_url=os.getenv('BROKER_URL'), loop=None):
     """
     :param exchange_name: optional to use but if no value sets its will be declare default exchange
